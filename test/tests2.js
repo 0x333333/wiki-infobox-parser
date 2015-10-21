63064f586115c272bff96682711a2dc3add025a2
Zhipeng Jiang <jesusjzp@gmail.com>
2015-10-21 02:13:56 +0200

    Add ~1k test case.

---
+++ test/tests2.js
@@ -0,0 +1,42 @@
+var assert = require("assert");
+var parser = require('../index');
+var checkJson = require('../lib/utils').checkJson;
+
+/**
+ * Load all keywords
+ * from './data/actors.txt'
+ */
+var fs = require('fs');
+var buffer, arr;
+
+fs.readFile(__dirname + '/data/actors.txt', function (err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    buffer = data.toString();
+    arr = buffer.split('\n');
+    test(arr);
+  }
+});
+
+/**
+ * Main test function
+ * @method test
+ * @param  {[Array]}  arr  Array of keywords
+ */
+function test(arr) {
+  describe('Stability test', function(done) {
+    arr.forEach(function(a) {
+      it('Should parse without crashing', function(done) {
+        parser(a, function(err, result) {
+          if (err) {
+            assert.equal('object', typeof err);
+          } else {
+            assert.equal(true, checkJson(result));
+          }
+          done();
+        });
+      });
+    });
+  });
+}
